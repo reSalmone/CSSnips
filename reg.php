5915b579216b43030f5f87ed9ea94ec4bdf6fb64
reSalmone <livioboccod@gmail.com>
2025-04-12 20:36:55 +0200

    initializing the rep

---
+++ reg.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrazione</title>
+</head>
+<body>
+    <?php
+        $_GET["qua ce piazzi il nome dell'input che vuoi"]
+    
+    ?>
+</body>
+</html>

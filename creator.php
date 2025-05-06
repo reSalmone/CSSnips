a1943267214300a9d7fff0f0f23423ed6430c4db
reSalmone <livioboccod@gmail.com>
2025-05-06 16:51:51 +0200

    in pratica ho spostato tutte le navbar e tutte le pagine login/signup in file separati, importati poi con del codice php

--- creator.php
+++ creator.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$redirect = 'creator.php';
 ?>
 
 
@@ -18,148 +19,8 @@ session_start();
 </head>
 
 <body>
-    <div class="navbar">
-        <div class="left-navbar">
-            <a href="index.php" class="navbar-title">CSSnips</a>
-            <button class="nbutton" onclick="location.href='explorer.php'" type="button">
-                <span>Explore</span>
-                <img src="assets/search.png" class="nicon">
-            </button>
-            <button class="nbutton" onclick="location.href = 'challenges.html'" type="button">
-                <span>Challenges</span>
-                <img src="assets/target.png" class="nicon">
-            </button>
-            <button class="nbutton" onclick="location.href = 'creator.php'" type="button">
-                <span>Create</span>
-                <img src="assets/add.png" class="nicon">
-            </button>
-        </div>
-        <div class="right-navbar">
-            <?php
-            /*se trova in questa sessione un username settato significa che l'utente ha già una sessione attiva e vede logout,
-            se no gli fa vedere il bottone login che runna openLogin() che è una funzione che sta nel file login.js che mostra
-            il blocco con id #page da display: none; a display: block;*/
-            if (isset($_SESSION['username'])) {
-                echo "<button class='nbutton' type='button' onclick='location.href=\"logout.php?redirect=creator.php\";'>
-                                <span>Logout (" . $_SESSION['username'] . ")</span>
-                            </button>";
-            } else {
-                echo '<button class="nbutton" type="button" onclick="openLogin(event);">
-                                <span>Login</span>
-                            </button>';
-                echo '<button class="nbutton" type="button" onclick="openSignup(event);">
-                                <span>Signup</span>
-                            </button>';
-            }
-            ?>
-        </div>
-    </div>
-    <div class="center-div" id="login-center-div">
-        <div class="form-page" id="login-page">
-            <p class="form-title">Login</p>
-            <div class="form-server-error-container" id="login-server-error">
-                <?php
-                if (isset($_SESSION['login_error'])) {
-                    echo "<span>" . $_SESSION['login_error'] . "</span>";
-                    echo "<script>
-                        window.addEventListener('load', function() {
-                            openLogin(event);
-                            showLoginServerError();
-                        });
-                    </script>";
-                    unset($_SESSION['login_error']);
-                }
-                ?>
-            </div>
-            <form action="login.php?redirect=creator.php" method="POST" class="form-form"
-                onsubmit="return submitLoginForm(this);" novalidate>
-                <div class="form-input-and-error-container">
-                    <div class="form-input-container">
-                        <input type="text" class="form-input" placeholder="Username or email" name="username"
-                            spellcheck="false">
-                        <img src="assets/email.png" class="form-icon">
-                    </div>
-                    <div class="form-error-box">
-                        <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                        <span class="form-error-text"></span>
-                    </div>
-                </div>
-                <div class="form-input-and-error-container">
-                    <div class="form-input-container">
-                        <input type="password" class="form-input" placeholder="Password" name="password"
-                            spellcheck="false">
-                        <img src="assets/lock.png" class="form-icon form-show-password" onclick="showPassword(this);"
-                            title="Show password">
-                    </div>
-                    <div class="form-error-box">
-                        <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                        <span class="form-error-text"></span>
-                    </div>
-                </div>
-                <div class="form-remember-forgot">
-                    <label class="checkbox-container">
-                        <input type="checkbox" class="checkbox" id="checkbox" name="remember">
-                        <div class="display-checkbox"></div>
-                        <label class="checkbox-label remember" for="checkbox">Remember me</label>
-                    </label>
-                    <a class="form-forgot-password" href="">Forgot password?</a>
-                </div>
-                <input type="submit" class="form-button" value="Login">
-            </form>
-            <p class="form-switch-form">Don't have an account? <span onclick="openSignup(event);">Signup</span></p>
-        </div>
-    </div>
-    <div class="center-div" id="signup-center-div">
-        <div class="form-page" id="signup-page">
-            <p class="form-title">Sign up</p>
-            <form action="signup.php?redirect=creator.php" method="POST" class="form-form"
-                onsubmit="return submitSignupForm(this);" novalidate>
-                <div class="form-input-and-error-container">
-                    <div class="form-input-container">
-                        <input type="text" class="form-input" placeholder="Username" name="username" spellcheck="false">
-                        <img src="assets/user.png" class="form-icon">
-                    </div>
-                    <div class="form-error-box">
-                        <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                        <span class="form-error-text"></span>
-                    </div>
-                </div>
-                <div class="form-separator">
-                    <div class="form-input-and-error-container">
-                        <div class="form-input-container">
-                            <input type="email" class="form-input form-input-1" placeholder="Email" name="email"
-                                spellcheck="false">
-                            <img src="assets/email.png" class="form-icon">
-                        </div>
-                        <input type="email" class="form-input form-input-2" placeholder="Confirm email"
-                            name="confirmEmail" spellcheck="false">
-                        <div class="form-error-box">
-                            <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                            <span class="form-error-text"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="form-separator">
-                    <div class="form-input-and-error-container">
-                        <div class="form-input-container">
-                            <input type="password" class="form-input form-input-1" placeholder="Password"
-                                name="password" spellcheck="false">
-                            <img src="assets/lock.png" class="form-icon form-show-password"
-                                onclick="showPassword(this);" title="Show password">
-                        </div>
-                        <input type="password" class="form-input form-input-2" placeholder="Confirm password"
-                            name="confirmPassword" spellcheck="false">
-                        <div class="form-error-box">
-                            <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                            <span class="form-error-text"></span>
-                        </div>
-                    </div>
-                </div>
-                <input type="submit" class="form-button" value="Sign up">
-            </form>
-            <p class="form-switch-form">Already have an account? <span onclick="openLogin(event);">Login</span></p>
-        </div>
-    </div>
+    <?php include 'navbar-code.php'; ?> <!--NAVBAR-->
+    <?php include 'login-signup-code.php'; ?> <!--LOGIN AND SIGNUP-->
     <div class="center-div post-center-div" id="post-center-div">
         <div class="post-page">
             <div class="post-title-container">
--- index.php
+++ index.php
@@ -7,8 +7,9 @@ ora dovrebbe funge <3
 
 
 <?php
-    //qua in pratica con session_start() pija le info dell'ultima sessione da un file che si è salvato
-    session_start();
+//qua in pratica con session_start() pija le info dell'ultima sessione da un file che si è salvato
+session_start();
+$redirect = 'index.php';
 ?>
 
 <!DOCTYPE html>
@@ -26,152 +27,8 @@ ora dovrebbe funge <3
 </head>
 
 <body>
-<div class="navbar">
-        <div class="left-navbar">
-            <a href="index.php" class="navbar-title">CSSnips</a>
-            <button class="nbutton" onclick="location.href='explorer.php'" type="button">
-                <span>Explore</span>
-                <img src="assets/search.png" class="nicon">
-            </button>
-            <button class="nbutton" onclick="location.href = 'challenges.html'" type="button">
-                <span>Challenges</span>
-                <img src="assets/target.png" class="nicon">
-            </button>
-            <button class="nbutton" onclick="location.href = 'creator.php'" type="button">
-                <span>Create</span>
-                <img src="assets/add.png" class="nicon">
-            </button>
-        </div>
-        <div class="right-navbar">
-            <?php
-            /*se trova in questa sessione un username settato significa che l'utente ha già una sessione attiva e vede logout,
-            se no gli fa vedere il bottone login che runna openLogin() che è una funzione che sta nel file login.js che mostra
-            il blocco con id #page da display: none; a display: block;*/
-            if (isset($_SESSION['username'])) {
-                echo "<button class='nbutton' type='button' onclick='location.href=\"logout.php?redirect=index.php\";'>
-                                <span>Logout (" . $_SESSION['username'] . ")</span>
-                            </button>";
-            } else {
-                echo '<button class="nbutton" type="button" onclick="openLogin(event);">
-                                <span>Login</span>
-                            </button>';
-                echo '<button class="nbutton" type="button" onclick="openSignup(event);">
-                                <span>Signup</span>
-                            </button>';
-            }
-            ?>
-        </div>
-    </div>
-    <div class="center-div" id="login-center-div">
-    <div class="form-page" id="login-page">
-        <p class="form-title">Login</p>
-        <div class="form-server-error-container" id="login-server-error">
-            <?php
-                if (isset($_SESSION['login_error'])) {
-                    echo "<span>" . $_SESSION['login_error'] . "</span>";
-                    echo "<script>
-                        window.addEventListener('load', function() {
-                            openLogin(event);
-                            showLoginServerError();
-                        });
-                    </script>";
-                    unset($_SESSION['login_error']);
-                }
-            ?>
-        </div>
-        <form action="login.php?redirect=index.php" method="POST" class="form-form" onsubmit="return submitLoginForm(this);" novalidate>
-            <div class="form-input-and-error-container">
-                <div class="form-input-container">
-                    <input type="text" class="form-input" placeholder="Username or email" name="username" spellcheck="false">
-                    <img src="assets/email.png" class="form-icon">
-                </div>
-                <div class="form-error-box">
-                    <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                    <span class="form-error-text"></span>
-                </div>
-            </div>
-            <div class="form-input-and-error-container">
-                <div class="form-input-container">
-                    <input type="password" class="form-input" placeholder="Password" name="password" spellcheck="false">
-                    <img src="assets/lock.png" class="form-icon form-show-password" onclick="showPassword(this);" title="Show password">
-                </div>
-                <div class="form-error-box">
-                    <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                    <span class="form-error-text"></span>
-                </div>
-            </div>
-            <div class="form-remember-forgot">
-                <label class="checkbox-container">
-                    <input type="checkbox" class="checkbox" id="checkbox" name="remember">
-                    <div class="display-checkbox"></div>
-                    <label class="checkbox-label remember" for="checkbox">Remember me</label>
-                </label>
-                <a class="form-forgot-password" href="">Forgot password?</a>
-            </div>
-            <input type="submit" class="form-button" value="Login">
-        </form>
-        <p class="form-switch-form">Don't have an account? <span onclick="openSignup(event);">Signup</span></p>
-    </div>
-    </div>
-    <div class="center-div" id="signup-center-div">
-    <div class="form-page" id="signup-page">
-        <p class="form-title">Sign up</p>
-        <div class="form-server-error-container" id="signup-server-error">
-            <?php
-                if (isset($_SESSION['signup_error'])) {
-                    echo "<span>" . $_SESSION['signup_error'] . "</span>";
-                    echo "<script>
-                        window.addEventListener('load', function() {
-                            openSignup(event);
-                            showSignupServerError();
-                        });
-                    </script>";
-                    unset($_SESSION['signup_error']);
-                }
-            ?>
-        </div>
-        <form action="signup.php?redirect=index.php" method="POST" class="form-form" onsubmit="return submitSignupForm(this);" novalidate>
-            <div class="form-input-and-error-container">
-                <div class="form-input-container">
-                    <input type="text" class="form-input" placeholder="Username" name="username" spellcheck="false">
-                    <img src="assets/user.png" class="form-icon">
-                </div>
-                <div class="form-error-box">
-                    <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                    <span class="form-error-text"></span>
-                </div>
-            </div>
-            <div class="form-separator">
-                <div class="form-input-and-error-container">
-                    <div class="form-input-container">
-                        <input type="email" class="form-input form-input-1" placeholder="Email" name="email" spellcheck="false">
-                        <img src="assets/email.png" class="form-icon">
-                    </div>
-                    <input type="email" class="form-input form-input-2" placeholder="Confirm email" name="confirmEmail" spellcheck="false">
-                    <div class="form-error-box">
-                        <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                        <span class="form-error-text"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="form-separator">
-                <div class="form-input-and-error-container">
-                    <div class="form-input-container">
-                        <input type="password" class="form-input form-input-1" placeholder="Password" name="password" spellcheck="false">
-                        <img src="assets/lock.png" class="form-icon form-show-password" onclick="showPassword(this);" title="Show password">
-                    </div>
-                    <input type="password" class="form-input form-input-2" placeholder="Confirm password" name="confirmPassword" spellcheck="false">
-                    <div class="form-error-box">
-                        <img src="assets/exclamation.png" class="form-icon form-error-icon">
-                        <span class="form-error-text"></span>
-                    </div>
-                </div>
-            </div>
-            <input type="submit" class="form-button" value="Sign up">
-        </form>
-        <p class="form-switch-form">Already have an account? <span onclick="openLogin(event);">Login</span></p>
-    </div>
-    </div>
+    <?php include 'navbar-code.php'; ?> <!--NAVBAR-->
+    <?php include 'login-signup-code.php'; ?> <!--LOGIN AND SIGNUP-->
     <div id="rest" onclick="closeLogin(); closeSignup();">
         <div class="first-container">
             <div class="title-container">
@@ -232,4 +89,5 @@ ora dovrebbe funge <3
 <script src="assets/scripts/title-wave2.js"></script>
 <script src="assets/scripts/login.js"></script>
 <script src="assets/scripts/signup.js"></script>
+
 </html>

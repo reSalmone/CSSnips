d50988c55eec86e4c66a3fef48b805ca8e450c92
giucic03 <giuseppeciccone2003@gmail.com>
2025-05-14 18:46:20 +0200

    ho aggiunto la parte per segnare nell account il numero corretto di file salvati e a cui si è messo like prendendo il numero dalla table users

--- account.php
+++ account.php
@@ -9,11 +9,18 @@ $redirect = 'account.php';
 
 $username = $_SESSION['username'] ?? '';
 $dbcon = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=alfonzo1") or die('Could not connect: ' . pg_last_error());
-$query = "SELECT username, email, bio FROM users WHERE username='$username';";
-$result = pg_query($query) or die('Query failed: '. pg_last_error());
+$query1 = "SELECT username, email, bio FROM users WHERE username='$username';";
+$query2 = "SELECT cardinality(likedsnippets) AS numero_stringhe FROM users WHERE username = '$username'";
+$query3 = "SELECT cardinality(savedsnippets) AS numero_stringhe FROM users WHERE username = '$username'";
+$result1 = pg_query($query1) or die('Query failed: '. pg_last_error());
+$result2 = pg_query($query2) or die('Query failed: '. pg_last_error());
+$result3 = pg_query($query3) or die('Query failed: '. pg_last_error());
 
-$line = pg_fetch_array($result, NULL, PGSQL_ASSOC);
-if (!$line) {
+$line1 = pg_fetch_array($result1, NULL, PGSQL_ASSOC);
+$line2 = pg_fetch_array($result2, NULL, PGSQL_ASSOC);
+$line3 = pg_fetch_array($result3, NULL, PGSQL_ASSOC);
+
+if (!$line1 or !$line2 or !$line3) {
   echo "<p>Could not find username.</p>";
   exit;
 }
@@ -49,8 +56,8 @@ if (!$line) {
           <img src="fotoprofilo.jpg" alt="Foto Profilo" class="profilo-img">
           <div class="info-text">
             <?php echo "<h2>" . htmlspecialchars($username) . "</h2>" ?>
-            <?php echo "<p>Email:" . htmlspecialchars($line['email']) . "</p>" ?>
-            <?php echo "<p>Bio:" . htmlspecialchars($line['bio']) . "</p>" ?>
+            <?php echo "<p>Email:" . htmlspecialchars($line1['email']) . "</p>" ?>
+            <?php echo "<p>Bio:" . htmlspecialchars($line1['bio']) . "</p>" ?>
           </div>
         </section>
 
@@ -68,8 +75,12 @@ if (!$line) {
             <div class="funzione-count">25</div>
           </div>
           <div class="funzione-box">
+            <div class="funzione-text">Likes</div>
+            <div class="funzione-count"><?php echo htmlspecialchars($line2['numero_stringhe']) ?></div>
+          </div>
+          <div class="funzione-box">
             <div class="funzione-text">Watchlist</div>
-            <div class="funzione-count">10</div>
+            <div class="funzione-count"><?php echo htmlspecialchars($line3['numero_stringhe']) ?></div>
           </div>
           <div class="funzione-box">
             <div class="funzione-text">Followers</div>
@@ -86,7 +97,9 @@ if (!$line) {
     <?php include 'footer-code.php'; ?> <!--FOOTER-->
   </div>
   <?php
-    pg_free_result($result);
+    pg_free_result($result1);
+    pg_free_result($result2);
+    pg_free_result($result3);
     pg_close($dbcon);
     ?>
 </body>

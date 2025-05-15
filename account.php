c6ec139109420fddadc41d50fcac884e7a3929a5
giucic03 <giuseppeciccone2003@gmail.com>
2025-05-16 00:10:33 +0200

    nella descrizione
    ho fatto la stessa cosa di ieri però per il numero di codici che l'utente ha fatto. poi ho quasi finito la parte sulle recent activity però la parte copiata da explorer non funziona probabilemente perchè manca qualcosa da importare. intanto ho solo printato i nomi dei file

--- account.php
+++ account.php
@@ -12,15 +12,21 @@ $dbcon = pg_connect("host=localhost port=5432 dbname=postgres user=postgres pass
 $query1 = "SELECT username, email, bio FROM users WHERE username='$username';";
 $query2 = "SELECT cardinality(likedsnippets) AS numero_stringhe FROM users WHERE username = '$username'";
 $query3 = "SELECT cardinality(savedsnippets) AS numero_stringhe FROM users WHERE username = '$username'";
+$query4 = "SELECT count(*) AS numero_codici FROM snips WHERE creator = '$username'";
+$query5 = "with this as (select * from snips where creator = 'test1' order by created_at limit 4) select file_location from this order by created_at desc";
 $result1 = pg_query($query1) or die('Query failed: '. pg_last_error());
 $result2 = pg_query($query2) or die('Query failed: '. pg_last_error());
 $result3 = pg_query($query3) or die('Query failed: '. pg_last_error());
+$result4 = pg_query($query4) or die('Query failed: '. pg_last_error());
+$result5 = pg_query($query5) or die('Query failed: '. pg_last_error());
 
 $line1 = pg_fetch_array($result1, NULL, PGSQL_ASSOC);
 $line2 = pg_fetch_array($result2, NULL, PGSQL_ASSOC);
 $line3 = pg_fetch_array($result3, NULL, PGSQL_ASSOC);
+$line4 = pg_fetch_array($result4, NULL, PGSQL_ASSOC);
+$line5 = pg_fetch_array($result5, NULL, PGSQL_ASSOC);
 
-if (!$line1 or !$line2 or !$line3) {
+if (!$line1 or !$line2 or !$line3 or !$line4 or !$line5) {
   echo "<p>Could not find username.</p>";
   exit;
 }
@@ -65,6 +71,15 @@ if (!$line1 or !$line2 or !$line3) {
         <section class="recent-activity">
           <h2>RECENT ACTIVITY</h2>
           <div class="activity-container">
+            <?php 
+              if(!$line5){
+                echo "<p>Non ci sono risultati.</p>";
+              }
+
+              do{
+                echo "". htmlspecialchars($line5["file_location"]) . "\t";
+              } while ($line5 = pg_fetch_array($result5, NULL, PGSQL_ASSOC));
+            ?>
           </div>
         </section>
 
@@ -72,7 +87,7 @@ if (!$line1 or !$line2 or !$line3) {
         <section class="funzioni-container">
           <div class="funzione-box">
             <div class="funzione-text">Activity</div>
-            <div class="funzione-count">25</div>
+            <div class="funzione-count"><?php echo htmlspecialchars($line4['numero_codici']) ?></div>
           </div>
           <div class="funzione-box">
             <div class="funzione-text">Likes</div>
@@ -100,6 +115,8 @@ if (!$line1 or !$line2 or !$line3) {
     pg_free_result($result1);
     pg_free_result($result2);
     pg_free_result($result3);
+    pg_free_result($result4);
+    pg_free_result($result5);
     pg_close($dbcon);
     ?>
 </body>

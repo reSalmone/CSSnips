251fd9a365f602ed83cbfafc7ac92ab47b5399f4
Ema2803 <emanuele.cola2003@gmail.com>
2025-05-28 18:16:56 +0200

    non so perchè ma ho dovuto togliere questi a mano

--- assets/scripts/challenges.js
+++ assets/scripts/challenges.js
@@ -3,7 +3,6 @@ const SlideLeft=document.getElementById('s-left');
 const SlideRight=document.getElementById('s-right');
 
 SlideLeft.addEventListener('click',()=>{Carosello.scrollLeft -=500;});
-SlideRight.addEventListener('click',()=>{Carosello.scrollLeft +=500;});SlideRight.addEventListener('click',()=>{Carosello.scrollLeft +=500;});
 SlideRight.addEventListener('click',()=>{Carosello.scrollLeft +=500;});
 
 
